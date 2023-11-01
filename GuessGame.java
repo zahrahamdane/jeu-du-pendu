04be5a3ca168fddc745fe838c0fd83ab26072874
Zahra Hamdane <hamdane.zahra@gmail.com>
2023-11-02 00:42:27 +0100

    Améliorations du jeu

--- GuessGame.java
+++ GuessGame.java
@@ -1,13 +1,39 @@
 package com.zaradev.pendu;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * Class responsible of representing the Guess Game. Provides methods to :
+ * - validate if the game is won or lost
+ * - validate if a given letter is considered discovered or not in the secret word
+ */
 public class GuessGame {
-	private final List<Character> secretWord = new ArrayList<>();
-	private int lifePoints;
-	private List<Character> guessWord = new ArrayList<>();	
 	
+	/**
+	 * Stores the secret word that the player wants to discover
+	 */
+	private final List<Character> secretWord = new ArrayList<>();
+	  /**
+     * Stores the remaining number of life points.
+     */
+	private int lifePoints;
+	/**
+     * Stores letters discovered by the player. '_' stored for not discovered letters.
+     */
+	private List<Character> guessWord = new ArrayList<>();	
+	 /**
+     * Stores letters that the player has used to try to discover the secret word.
+     */
+	private Set<Character> guessLetter = new HashSet<>();
+	
+	/**
+     * Build a Guess Game object.
+     * @param secretWord the secret word the player has to discover.
+     * @param lifePoints the number of retries allowed to discover the secret word.
+     */
 	public GuessGame(String wordToGuess, int lifePoints) {
 		for(char c : wordToGuess.toCharArray()) {
 			this.secretWord.add(c);
@@ -18,15 +44,14 @@ public class GuessGame {
 		for(int index = 0; index < secretWord.size(); index++) {
 			this.guessWord.add('_');
 		}
-		
 	}
 
-	@Override
-	public String toString() {
-		return "GuessGame [lifePoints=" + lifePoints + ", guessWord=" + guessWord + "]";
-	}
-
+	 /**
+     * Algorithm which verifies if a char given by the player is discovered in the secret word.
+     * @param letter the letter to validate in `secretWord` and `guessWord`.
+     */
 	public void guessLetter(char letter) {
+		guessLetter.add(letter);
 		if(secretWord.contains(letter) && !guessWord.contains(letter)) {
 			int index = 0;
 			for(char c : secretWord) {
@@ -36,19 +61,29 @@ public class GuessGame {
 				index++;
 			}	
 		}else {
-			lifePoints -=1;
-		}
-		
+			lifePoints -= 1;
+		}	
 	}
 
+	 /**
+     * Check if the game is lost.
+     * @return boolean true if the game is lost, false otherwise.
+     */
 	public boolean isLost() {
 		return lifePoints <= 0;
 	}
 
+	  /**
+     * Check if the game is won.
+     * @return boolean true is the game is won, false otherwise.
+     */
 	public boolean isWon() {
 		return ! guessWord.contains('_');
 	}
 
-	
+	@Override
+	public String toString() {
+		return "GuessGame [lifePoints=" + lifePoints + ", guessWord=" + guessWord + ", guessLetter=" + guessLetter + "]";
+	}
 
 }
--- Main.java
+++ Main.java
@@ -3,13 +3,18 @@ package com.zaradev.pendu;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Class of the entrypoint of the Guess Game.
+ */
 public class Main {
-
+	
+	  /**
+     * Entry point of the Guess Game. Contains the main algorithm of the game.
+     */
 	public static void main(String[] args) {
-		final Scanner scanner = new Scanner(System.in);
 		final Random random = new Random();
 		final String[] words = "abuser crottes fleches continental babiole etoile bougie coup coeur malade".split(" ");
-		final String wordToGuess = words[random.nextInt(words.length)];
+		String wordToGuess = words[random.nextInt(words.length)];
 		
 		GuessGame game = new GuessGame(wordToGuess, 10);
 		
@@ -17,25 +22,46 @@ public class Main {
 		
 		while(true) {
 			System.out.println(game);
-			System.out.println("Entrez une lettre :");
 			
-			final char letter = scanner.nextLine().charAt(0);
+			final char letter = scanLetter("Entrez une lettre :");
 			
 			game.guessLetter(letter);
 			
 			if(game.isLost()) {
 				System.out.println(game);
 				System.out.println("Perdu !");
-				break;
 			}
 			
 			if(game.isWon()) {
 				System.out.println(game);
 				System.out.println("Gagne !");
-				break;
+			}
+			
+			if(game.isLost() || game.isWon()) {
+				char replayAnswer = scanLetter("Voulez-vous rejouer ?");
+				if(replayAnswer == 'y' || replayAnswer == 'Y' || replayAnswer == 'o' || replayAnswer == 'O') {
+					wordToGuess = words[random.nextInt(words.length)];
+					game = new GuessGame(wordToGuess, 10);
+				}else {
+					break;
+				}
 			}
 		}
 
+	}
+
+	private static char scanLetter(String question) {
+		final Scanner scanner = new Scanner(System.in);
+		Character letter = null;
+		do {
+			System.out.println(question);
+			String input = scanner.nextLine();
+			if(input.length() == 1) {
+				letter = input.charAt(0);
+			}
+		}while(letter == null);
+		
+		return letter;
 	}
 
 }
